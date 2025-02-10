8b62ba081e19815085858ed20b11dde8d86cf562
romiyusnandar <yusromi04@gmail.com>
2025-02-10 07:43:21 +0000

    Settings: Build RyuLab
    Change-Id: I30f6c5ceb76c42c9ddb33f7df9d4b4bc9d14682e
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -211,6 +211,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.android.settings.livedisplay.LiveDisplaySettings;
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
+import org.ryuui.lab.RyuLab;
 
 public class SettingsGateway {
 
@@ -411,6 +412,8 @@ public class SettingsGateway {
             LiveDisplaySettings.class.getName(),
             WifiTetherClientManager.class.getName(),
             WirelessDebuggingFragment.class.getName()
+            // Ryu-UI Org additions
+            RyuLab.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -458,5 +461,7 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.WirelessDebuggingActivity.class.getName(),
+            // Ryu-UI Org additions
+            Settings.RyuLabSettingsActivity.class.getName(),
     };
 }
